0a930e2616d043e3d5f655a8e918ca18db864b90
silvelo <arturo.silvelo@gmail.com>
2017-05-18 11:05:01 +0200

    Add enviroments

--- server/src/config/settings.ts
+++ server/src/config/settings.ts
@@ -1,6 +1,15 @@
 import { ISettings } from './models';
 
-export const SETTINGS: ISettings = {
+const isProd = process.env.NODE_ENV === 'production';
+
+const settingsProd: ISettings = {
+    port: 2000,
+    secret: 'secret',
+};
+
+const settingsDev: ISettings = {
     port: 3000,
     secret: 'secret',
 };
+
+export const SETTINGS = (isProd) ? settingsProd : settingsDev;
