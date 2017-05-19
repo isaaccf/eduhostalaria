6face4d339e1de4436014263be0325013e89da7b
Alberto Basalo <albertobasalo@hotmail.com>
2017-05-19 20:13:00 +0200

    layout routing

--- client/src/app/core/layout/layout-routing.module.ts
+++ client/src/app/core/layout/layout-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './../../routes/home/home.module#HomeModule'
+  },
+  {
+    path: 'admin',
+    loadChildren: './../../routes/admin/admin.module#AdminModule'
+  },
+  {
+    path: 'bookings',
+    loadChildren: './../../routes/bookings/bookings.module#BookingsModule'
+  },
+  {
+    path: 'client',
+    loadChildren: './../../routes/client/client.module#ClientModule'
+  },
+  {
+    path: 'login',
+    loadChildren: './../../routes/login/login.module#LoginModule'
+  },
+  {
+    path: 'org/:organizationName',
+    loadChildren: './../../routes/organization/organization.module#OrganizationModule'
+  },
+  {
+    path: 'organizer',
+    loadChildren: './../../routes/organizer/organizer.module#OrganizerModule'
+  },
+  {
+    path: 'usher',
+    loadChildren: './../../routes/usher/usher.module#UsherModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
