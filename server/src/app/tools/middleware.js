37ac087e31969cb5de0e3a80c57c50b3385ce389
Alberto Basalo <albertobasalo@hotmail.com>
2017-08-18 13:28:23 +0200

    logger and router

--- server/src/app/tools/middleware.js
+++ server/src/app/tools/middleware.js
@@ -5,8 +5,13 @@
  */
 module.exports.useMiddleware = app => {
   const cors = require('cors');
+  const logger = require('morgan');
   const bodyParser = require('body-parser');
   const security = require('./security/security.service');
+  app.disable('x-powered-by');
+  app.use(logger('dev', {
+    skip: () => app.get('env') === 'test'
+  }));
 
   app.use(cors());
 
@@ -15,10 +20,21 @@ module.exports.useMiddleware = app => {
   }));
   app.use(bodyParser.json());
 
-  app.use((req, res, next) => {
-    console.log(`req : ${req.method} - ${req.url}`);
-    next();
-  });
+  require('../lib/index.controller')(app);
 
   security.useSecurity(app, '/api/priv/');
+
+
+  app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+  app.use((err, req, res, next) => {
+    res
+      .status(err.status || 500)
+      .send({
+        message: err.message
+      });
+  });
 }
--- server/src/main.js
+++ server/src/main.js
@@ -3,7 +3,6 @@ const app = express();
 
 const middleware = require('./app/tools/middleware');
 middleware.useMiddleware(app);
-require('./app/lib/index.controller')(app);
 
-app.listen(2000);
-console.log('listening on port 2000');
+const { PORT = 2000 } = process.env;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); 
