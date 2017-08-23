13982d5d813bd4220717dfdb25e4d93ff97d1dad
Alberto Basalo <albertobasalo@hotmail.com>
2017-08-23 18:14:43 +0200

    rest utils

--- server/src/app/tools/rest.utils.js
+++ server/src/app/tools/rest.utils.js
@@ -2,23 +2,14 @@ const logger = require('winston');
 
 module.exports.isError = data => data instanceof Error;
 
-module.exports.returnErr = (err, res) => {
-  logger.debug(JSON.stringify(err));
-  let code = 400;
-  if (err.code === 11000) {
-    code = 409;
-  }
-  return res.status(code).json({ err: err.message });
-};
-
 module.exports.returnData = (data, res) => {
   logger.debug(JSON.stringify(data));
   return res.status(200).json(data);
 };
 
-module.exports.returnNotFound = (res) => {
-  logger.debug('Something not found');
-  return res.status(404).send();
+module.exports.returnInserted = (data, res) => {
+  logger.debug(JSON.stringify(data));
+  return res.status(201).json(data);
 };
 
 module.exports.returnEmpty = (res) => {
@@ -26,7 +17,16 @@ module.exports.returnEmpty = (res) => {
   return res.status(204).send();
 };
 
-module.exports.returnInserted = (data, res) => {
-  logger.debug(JSON.stringify(data));
-  return res.status(201).send(data);
+module.exports.returnNotFound = (res) => {
+  logger.debug('Something not found');
+  return res.status(404).send();
+};
+
+module.exports.returnErr = (err, res) => {
+  logger.debug(JSON.stringify(err));
+  let code = 400;
+  if (err.code === 11000) {
+    code = 409;
+  }
+  return res.status(code).json({ err: err.message });
 };
