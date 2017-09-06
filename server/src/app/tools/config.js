f1eddc714351e787e1dffc7a8e32e31681470b22
silvelo <arturo.silvelo@gmail.com>
2017-09-06 12:02:12 +0200

    convert to values

--- server/src/app/tools/config.js
+++ server/src/app/tools/config.js
@@ -8,10 +8,10 @@ config.secret = env.secret || dev.secret;
 config.MONGODB_URI = env.MONGODB_URI || dev.MONGODB_URI;
 config.URLBASE = env.URLBASE || dev.URLBASE;
 config.mailer = dev.mailer;
-config.mailer.port = env.MAILER_PORT || config.mailer.port;
+config.mailer.port = parseInt(env.MAILER_PORT, 10) || config.mailer.port;
 config.mailer.host = env.MAILER_HOST || config.mailer.host;
-config.mailer.secure = env.MAILER_SECURE || config.mailer.secure;
-config.mailer.ignoreTLS = env.MAILER_TLS || config.mailer.ignoreTLS;
+config.mailer.secure = Boolean.valueOf(env.MAILER_SECURE) || config.mailer.secure;
+config.mailer.ignoreTLS = Boolean.valueOf(env.MAILER_TLS) || config.mailer.ignoreTLS;
 config.mailer.auth.user = env.MAILER_USER || config.mailer.auth.user;
 config.mailer.auth.pass = env.MAILER_PASS || config.mailer.auth.pass;
 
