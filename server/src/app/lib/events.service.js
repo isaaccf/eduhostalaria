110e0df10ce48e3bcd85742d93836dee1f3fb12e
silvelo <arturo.silvelo@gmail.com>
2017-09-08 11:35:49 +0200

    Update events and get One

--- server/src/app/lib/events.service.js
+++ server/src/app/lib/events.service.js
@@ -6,11 +6,13 @@ function _id(id) {
   return new ObjectID(id);
 }
 
-module.exports.getAll = async (userId) => {
+module.exports.getAll = async (organizationId, ownerId) => {
   const options = {};
-  if (userId) { options.userId = userId; }
+  if (organizationId) { options.organizationId = organizationId; }
+  if (ownerId) { options.ownerId = ownerId; }
   return mongo.find(col, options);
 };
 
 module.exports.insertEvent = async (event) => mongo.insertOne(col, event);
-
+module.exports.updateEvent = async (eventId, event) => mongo.updateOne(col, eventId, event);
+module.exports.getById = async eventId => mongo.findOneById(col, eventId);
