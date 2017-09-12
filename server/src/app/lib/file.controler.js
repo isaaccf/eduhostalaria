3c79b15f600c67b9cb876c2e7b0ad73cc4271bb8
silvelo <arturo.silvelo@gmail.com>
2017-09-12 13:49:46 +0200

    Cloudinary

--- server/src/app/lib/file.controler.js
+++ server/src/app/lib/file.controler.js
@@ -1,14 +1,24 @@
 const rest = require('../tools/rest.service');
 const multer = require('multer');
+const cloudinaryStorage = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary');
 
-const upload = multer({ dest: 'uploads/' });
-const logger = require('winston');
+const storage = cloudinaryStorage({
+  cloudinary,
+  filename: (req, file, cb) => {
+    cb(undefined, file.originalname);
+  },
+});
+
+const parser = multer({ storage });
 
 module.exports = (app, url) => {
   app.route(`${url}`)
-    .post(upload.any(), (req, res) => {
-      console.log(__dirname);
-      req.files.forEach(element => console.log(element));
+    .post(parser.any(), (req, res) => {
+      req.files.forEach(e => {
+        console.log(e.url);
+        console.log(e.secure_url);
+      });
       return rest.returnArray([], res);
     });
 };
