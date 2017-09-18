aa3e2bafd67806d452e9670f3665a64f5424ad55
elias-garcia <elias.garcia@udc.es>
2017-09-18 12:06:39 +0200

    Closes #32

--- client/src/app/views/me/events/events.component.ts
+++ client/src/app/views/me/events/events.component.ts
@@ -43,7 +43,6 @@ export class EventsComponent implements OnInit {
   getEvents() {
     this.me.getEvents().subscribe(events => {
       this.events = events;
-      console.log(events);
     });
   }
 
