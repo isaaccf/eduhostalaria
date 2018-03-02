4b703e3fe4e9c6c16de7a61200d85f50a3c59e28
elias-garcia <eliasgarciadev@gmail.com>
2018-03-02 09:41:38 +0100

    Event text refactoring

--- client/src/app/views/me/me.routing.ts
+++ client/src/app/views/me/me.routing.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
   {
     path: 'events',
     component: EventsComponent,
-    data: { name: 'me_events', title: 'Eventos' }
+    data: { name: 'me_events', title: 'Ofertas' }
   },
   {
     path: 'bookings',
