c7ca558fbf8ec61a5a8f5810420aaecf165c080d
elias-garcia <eliasgarciadev@gmail.com>
2018-04-19 14:04:01 +0200

    Informes

--- client/src/app/views/me/event-print/event-print.component.ts
+++ client/src/app/views/me/event-print/event-print.component.ts
@@ -1,30 +1,41 @@
-import { Component, OnInit, AfterViewInit, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterViewChecked, Inject } from '@angular/core';
 import { BusService } from '../../../tools/bus.service';
 import { MeService } from '../me.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { SecurityService } from '../../../tools/security.service';
+
+declare var window: any;
 
 @Component({
   selector: 'ab-event-print',
   templateUrl: './event-print.component.html',
-  styles: []
+  styleUrls: ['./event-print.component.css']
 })
 export class EventPrintComponent implements OnInit {
 
   public event: any;
   public bookings: any;
+  public organization: any;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private busService: BusService,
-    private meService: MeService
+    private meService: MeService,
+    private securityService: SecurityService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.busService.emitIsPrintingMode(true);
+    this.getOrganization();
     this.activatedRoute.params.subscribe((params: Params) => {
       this.getEvent(params.id);
       this.getBookings(params.id);
     });
+  }
+
+  getOrganization() {
+    this.organization = this.securityService.getLocalOrganization();
   }
 
   getEvent(eventId: string) {
@@ -36,11 +47,26 @@ export class EventPrintComponent implements OnInit {
   }
 
   getBookings(eventId) {
-    this.meService.getBookingsByEventId(eventId).subscribe(
-      (bookings) => {
-        this.bookings = bookings;
-      }
-    )
+    this.meService.getBookingsByEventId(eventId)
+      .map((bookings: any) => {
+        return bookings.sort((a, b) => {
+          return a.name > b.name;
+        });
+      })
+      .subscribe(
+        (bookings) => {
+          this.bookings = bookings;
+        }
+      )
+  }
+
+  onNavigateToHome() {
+    this.busService.emitIsPrintingMode(false);
+    this.router.navigateByUrl('');
+  }
+
+  onPrint() {
+    window.print();
   }
 
 }
