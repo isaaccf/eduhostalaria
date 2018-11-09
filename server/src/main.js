61b832cb911bd1b864e1d746c67fd6fcb89510d7
Alberto Basalo <albertobasalo@hotmail.com>
2018-11-09 14:26:59 +0100

    #74

--- server/src/main.js
+++ server/src/main.js
@@ -1,44 +1,53 @@
-const express = require('express');
-const config = require('./app/tools/config');
-const middleware = require('./app/tools/middleware');
-const logger = require('winston');
-const tracker = require('./app/tools/ga.service');
+const express = require("express");
+const config = require("./app/tools/config");
+const middleware = require("./app/tools/middleware");
+const logger = require("winston");
+const tracker = require("./app/tools/ga.service");
 
 logger.configure({
   transports: [
-    new (logger.transports.Console)({
-      level: 'debug',
+    new logger.transports.Console({
+      level: "debug",
       handleExceptions: true,
-      json: false,
-    }),
-  ],
+      json: false
+    })
+  ]
 });
 
-logger.level = 'debug';
+logger.level = "debug";
 
-process.on('uncaughtException', (err) => {
-  logger.warn('uncaughtException');
+process.on("uncaughtException", err => {
+  const msg = `uncaughtException: ${err.message || ""}`;
+  logger.warn("uncaughtException");
   logger.error(err);
+  tracker("express", msg);
   process.exit(1);
 });
 
-process.on('unhandledRejection', (err) => {
-  logger.warn(`unhandledRejection: ${err.message}`);
+process.on("unhandledRejection", err => {
+  const msg = `unhandledRejection: ${err.message || ""}`;
+  logger.warn(msg);
+  tracker("express", msg);
   logger.error(err);
 });
 
 logger.info(config);
 
 const app = express();
-const mongoService = require('./app/tools/mongo.service');
+const mongoService = require("./app/tools/mongo.service");
 
-mongoService.connect()
+mongoService
+  .connect()
   .then(() => {
     middleware.useMiddleware(app);
-    app.listen(config.port, () => logger.info(`Listening on port ${config.port}`));
+    app.listen(config.port, () => {
+      const msg = `Listening on port ${config.port}`;
+      tracker("Start", msg);
+      logger.info(msg);
+    });
   })
-  .catch((err) => {
-    tracker('Mongo', err.message);
-    logger.warn('No MongoDB');
+  .catch(err => {
+    tracker("No Mongo", err.message);
+    logger.warn("No MongoDB");
     logger.error(err);
   });
