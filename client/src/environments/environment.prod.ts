eb8e1322e8533691d7b6a2046955e8b6f2b577e0
Isaac Castro Fuentes <isaac@isaaccastro.eu>
2022-03-05 18:17:29 +0100

    Changed footer text

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  appName: 'Reservas',
-  appTitle: 'Reservas Hostalaría',
-  apiUrl: 'http://reservas.eduhostalaria.com/api/',
-  assetsUrl: 'http://reservas.eduhostalaria.com/',
-  secret: 'secret',
-  godEmail: 'admin@agorabinaria.com'
+  appName: "Reservas",
+  appTitle: "Reservas Hostalaría",
+  apiUrl: "https://reservas.eduhostalaria.com/api/",
+  assetsUrl: "https://reservas.eduhostalaria.com/",
+  secret: "secret",
+  godEmail: "eduhostalaria@outlook.es",
 };
 //  heroku assetsUrl: 'http://reservas.eduhostalaria.com/',
 //  local assetsUrl: 'http://localhost:2000/',
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,10 +1,9 @@
 export const environment = {
   production: false,
-  appName: 'Reservas',
-  appTitle: 'Reservas Escolas',
-  apiUrl: 'http://localhost:2000/api/',
-  assetsUrl: 'http://localhost:4200/',
-  secret: 'secret',
-  godEmail: 'admin@agorabinaria.com'
+  appName: "Reservas",
+  appTitle: "Reservas Escolas",
+  apiUrl: "http://localhost:2000/api/",
+  assetsUrl: "http://localhost:4200/",
+  secret: "secret",
+  godEmail: "eduhostalaria@outlook.es",
 };
-
